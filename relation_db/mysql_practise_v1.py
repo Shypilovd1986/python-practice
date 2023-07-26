04649486903c03e22d7e4717e8abc44d0e3b7c2c
shypilovd1986 <shypilovd@gmail.com>
2023-07-26 22:18:56 +0300

    practise sqlalchemy core

--- relation_db/mysql_practise_v1.py
+++ relation_db/mysql_practise_v1.py
@@ -1,42 +1,78 @@
+"""module for creating db and table """
+
 import mysql.connector as mysql
 
 
 def create_db(db_name, host, user, password):
+    """function for creating db"""
     try:
         con = mysql.connect(host=host, user=user, password=password)
         cur = con.cursor()
         cur.execute(f'CREATE DATABASE IF NOT EXISTS {db_name};')
         con.close()
-    except:
+    except mysql.Error:
         print('database cannot be created')
 
 
 def connect_to_db(db_name):
+    """function for connecting with db"""
     try:
         con = mysql.connect(host='localhost', user='root', password='19865421', database=db_name)
         print(f'You successfully connected to {db_name} database')
-    except:
+    except mysql.Error:
         print('You cannot connect to db')
     return con
 
 
 def create_table_teacher():
+    """for creating table teacher with columns id, name, surname, age"""
     con = connect_to_db('practise_v1')
-    con.cursor().execute('''CREATE TABLE IF NOT EXISTS teacher(id INT PRIMARY KEY AUTO_INCREMENT,
-                            name VARCHAR(20) NOT NULL,
-                            surname VARCHAR(20) NOT NULL, 
-                            age INT NOT NULL);''')
+    try:
+        con.cursor().execute('''CREATE TABLE IF NOT EXISTS teacher(id INT PRIMARY KEY AUTO_INCREMENT,
+                             name VARCHAR(20) NOT NULL,
+                             surname VARCHAR(20) NOT NULL, 
+                             age INT NOT NULL);''')
+    except mysql.Error:
+        print('Table cannot be created')
+    con.close()
 
 
 def create_table_pupil():
+    """for creating table pupil"""
     con = connect_to_db('practise_v1')
-    con.cursor().execute('''CREATE TABLE IF NOT EXISTS pupil(id INT PRIMARY KEY AUTO_INCREMENT,
-                        name VARCHAR(20) NOT NULL, 
-                        surname VARCHAR(20) NOT NULL,
-                        age INT NOT NULL);''')
+    try:
+        con.cursor().execute('''CREATE TABLE IF NOT EXISTS pupil(id INT PRIMARY KEY AUTO_INCREMENT,
+                            name VARCHAR(20) NOT NULL, 
+                            surname VARCHAR(20) NOT NULL,
+                            age INT NOT NULL);''')
+    except mysql.Error:
+        print('Table cannot be created')
+    con.close()
+
+
+def update_credential_teacher(teacher_id, name, surname, age,):
+    """for updating teacher"""
+    con = connect_to_db('practise_v1')
+    cur = con.cursor()
+    credentials_list = (name, surname, age, teacher_id)
+    cur.execute('''UPDATE teacher SET name=%s, surname=%s, age=%s WHERE id=%s''', credentials_list)
+    con.commit()
+    con.close()
+
+
+def read_all_items(table_name):
+    """for reading items from teacher table"""
+    con = connect_to_db('practise_v1')
+    cur = con.cursor()
+    cur.execute(f'''SELECT * FROM {table_name}''')
+    print(cur.lastrowid)
+    print(cur.fetchall())
+    con.close()
 
 
 if __name__ == '__main__':
     create_db('practise_v1', 'localhost', 'root', '19865421')
     create_table_teacher()
     create_table_pupil()
+    update_credential_teacher(4, 'Dmitriy', 'Shypilov', 37)
+    read_all_items('teacher')
---
+++ relation_db/mysql_practise_v2.py
@@ -0,0 +1,35 @@
+"""practise with sqlalchemy core"""
+from sqlalchemy import create_engine
+from sqlalchemy import Table, MetaData
+
+
+engine = create_engine('mysql+mysqlconnector://root:19865421@localhost:3306/practise_v1')
+
+# engine = create_engine('postgresql://postgres:19865421@localhost/practise_v1')
+
+
+with engine.connect() as connection:
+    meta = MetaData(engine)
+    teacher = Table('teacher', meta, autoload=True, autoload_with=engine)
+    show_all = teacher.select()
+    result = connection.execute(show_all)
+    for res in result:
+        print(res)
+
+    add_teacher = teacher.insert().values(
+        id=5,
+        name='Masha',
+        surname='Shypilova',
+        age=8
+    )
+
+    connection.execute(add_teacher)
+
+    print('**************************')
+
+    result = connection.execute(show_all)
+    for res in result:
+        print(res)
+
+    delete_Maria_teacher = teacher.delete().where(id == 5)
+    connection.execute(delete_Maria_teacher)
